dbbcbf398c6b305812a7434c009915d425bdf0da
Rochiyat <rochiyat@gmail.com>
2025-02-08 09:18:49 +0700

    Add Swagger documentation for API routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import env from './configs/env.config';
 import router from './routes/index';
+import { setupSwagger } from './swagger';
 
 const PORT = env.PORT;
 
@@ -9,10 +10,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+setupSwagger(app);
 app.use('/api', router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`API Documentation: http://localhost:${PORT}/api-docs`);
 });
 
 export default app;
